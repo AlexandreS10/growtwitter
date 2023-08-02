60058c9ec1b09c89b6c895ff4b300789b1fcce51
AlexandreS10 <alexandres10santos@gmail.com>
2023-08-02 12:47:18 -0300

    modifiquei o Readme e o index, realizando os testes das funcionalidades do projeto

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# growtwitter
+
+#GrowTwitter - Rede Social estilo Twitter com POO em TypeScript
+
+GrowTwitter é uma aplicação de rede social estilo Twitter desenvolvida em TypeScript, utilizando os conceitos de Programação Orientada a Objetos (POO). A aplicação possui classes para as entidades necessárias e estabelece relacionamentos do tipo composição. Os dados são armazenados em um banco de dados em memória (array).
+
+Requisitos:
+
+Node.js com TypeScript instalado.
+Funcionalidades
+GrowTwitter possui as seguintes funcionalidades:
+
+Cadastro de usuários
+Cadastro de tweets por usuários
+Mecanismo de seguidores entre usuários
+Likes e replies em tweets
+Entidades e Atributos
+A aplicação possui as seguintes entidades com seus respectivos atributos:
+
+#Usuário
+
+Identificador (id)
+Nome (name)
+E-mail (email)
+Username (username)
+Senha (password)
+Tweet
+Identificador (id)
+Conteúdo (content)
+Tipo (type) [base ou reply]
+
+#Regras e Funcionalidades Usuário
+
+Cada usuário possui um id e username únicos.
+Os usuários podem criar tweets.
+Os usuários podem seguir outros usuários.
+Os usuários podem curtir e responder (reply) tweets de outros usuários.
+Os usuários podem visualizar os tweets dos usuários que estão seguindo (estilo feed de tweets).
+
+#Tweet
+
+Cada tweet possui um id único.
+Os tweets podem ser do tipo "base" ou "reply".
+Cada tweet pertence a apenas um usuário.
+Os tweets podem conter likes e replies.
+Os tweets podem ser exibidos de acordo com as regras estabelecidas.
+
+#Follower
+
+Um usuário pode seguir outro usuário.
+O usuário que segue outro é chamado de "Follower".
+Um usuário pode visualizar os tweets de quem está seguindo.
+
+#Like e Reply
+
+Um usuário pode responder a um tweet específico com outro tweet (reply).
+Um tweet reply conterá a propriedade tipo setada como "Reply".
+Um usuário pode curtir um tweet específico.
+Um tweet pode ter zero ou vários likes e replies.
---
+++ src/index.ts
@@ -0,0 +1,56 @@
+import { Console, log } from "console";
+import ReplieController from "./controllers/ReplieController";
+import TweetController from "./controllers/TweetController";
+import UsuarioController from "./controllers/UsuarioController";
+import replies from "./data/replie";
+import usuarios from "./data/usuario";
+import Replie from "./model/Replie";
+import Usuario from "./model/Usuario";
+import { TweetBase } from "./model/TweetBase";
+import Follower from "./controllers/FollowController";
+import LikeController from "./controllers/LikeController";
+import FollowController from "./controllers/FollowController";
+import FeedController from "./controllers/FeedController";
+
+
+  const usuario1  =  new Usuario("1234","Alexandre","alexandre@growdev.com","xandão","12345");
+  const usuario2 = new Usuario("4321", "Misael", "misael@growdev.com", "misa", "12345")
+  const usuario3 = new Usuario("5678", "Jose", "jose@growdev.com", "ze", "20202")
+
+  usuarios.push(usuario1);
+  usuarios.push(usuario2);
+  usuarios.push(usuario3);
+
+  TweetController.addTweet("11","1234","Esse é meu primeiro tweet dentro desta plataforma")
+  TweetController.addTweet("847","1234","Esse é meu segundo tweet dentro desta plataforma")
+  TweetController.addTweet("12","4321","Tweet do Misael!!")
+  TweetController.addTweet("12","5678","Tweet do Jose!!")
+  TweetController.addTweet("12","5678","Tweet do Jose2!!")
+  TweetController.addTweet("12","5678","Tweet do Jose3!!")
+
+  LikeController.addLike("55","4321","11","1234")
+  LikeController.addLike("57","5678","847","1234")
+  LikeController.addLike("029","1234","12","5678")
+  LikeController.addLike("1435","1234","12","5678")
+
+ 
+
+
+  ReplieController.addReplie("22","4321","Este também é minha primeira vez aqui.","11","1234")
+  ReplieController.addReplie("89","5678","Replie do zé","11","1234")
+  ReplieController.addReplie("89","5678","Replie do zé","11","1234")
+  ReplieController.addReplie("21345","4321","Replie no Zé!!","12","5678")
+
+
+
+
+FollowController.follow("1234","4321")//primeiro parametro: id de quem eu quero seguir, segundo parametro: meu id
+FollowController.follow("5678","4321")//primeiro parametro: id de quem eu quero seguir, segundo parametro: meu id
+FollowController.follow("5678","1234")//primeiro parametro: id de quem eu quero seguir, segundo parametro: meu id
+FollowController.follow("4321","5678")//primeiro parametro: id de quem eu quero seguir, segundo parametro: meu id
+
+LikeController.addLike("5555","1234","11","1234")
+
+FeedController.mostrarFeed("4321")
+
+
